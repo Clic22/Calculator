b17ddd862fed552fbb2e6e06731ca314d06a7967
Clic22 <52575554+Clic22@users.noreply.github.com>
2024-07-27 21:28:15 +0000

    add first condition

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+def show_info(hide_version = False):
+    print("author: Carole Berjot")
+    if hide_version == False :
+        print("version number: 0.1")
+
+
+# Execution
+show_info(True)
 input1=input()
 print("result: "+input1)
